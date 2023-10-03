13fab69badfa7435cc559612c35c1cf40d58a900
MateusJSouza <mateusjdev@gmail.com>
2023-10-03 00:02:53 -0300

    fix: :lipstick: add box-shadow and animation

--- src/view/components/Sidebar/NavItem/index.tsx
+++ src/view/components/Sidebar/NavItem/index.tsx
@@ -9,10 +9,10 @@ export function NavItem({ title, icon: Icon }: NavItemProps) {
   return (
     <a
       href="#"
-      className="group flex w-full items-center gap-6 rounded-2xl px-6 py-4 hover:bg-indigo-600"
+      className="group flex w-full items-center gap-6 rounded-2xl px-6 py-4 transition-colors hover:bg-indigo-600 hover:shadow-[0px_20px_50px_0px_rgba(55,69,87,0.10)]"
     >
       <Icon className="h-8 w-8" />
-      <span className="text-greys-700 text-lg font-medium group-hover:font-bold group-hover:text-white">
+      <span className="text-greys-700 text-lg font-medium transition-colors group-hover:font-bold group-hover:text-white">
         {title}
       </span>
     </a>
