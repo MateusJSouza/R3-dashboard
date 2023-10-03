99c336299a36e36c0dc05347e819483357f42d03
MateusJSouza <mateusjdev@gmail.com>
2023-10-03 00:05:27 -0300

    feat: :sparkles: Sales and UsersCard component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import { Sidebar } from './view/components/Sidebar'
 import { Header } from './view/components/Header'
+import { Sales } from './view/components/Sales'
+import { UsersCard } from './view/components/UsersCard'
 
 export function App() {
   return (
     <div className="grid-cols-app grid min-h-screen bg-zinc-200">
       <Sidebar />
 
-      <main className="lg:col-start-2 lg:px-8 lg:pt-8">
+      <main className="bg-greys-300 col-start-2">
         <Header />
+
+        <div className="grid-cols-card grid gap-8 p-8">
+          <Sales />
+
+          <UsersCard />
+        </div>
       </main>
     </div>
   )
