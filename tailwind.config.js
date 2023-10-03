9c38077955990bc49b981b3c501e59e02185983b
MateusJSouza <mateusjdev@gmail.com>
2023-10-03 00:00:31 -0300

    feat: :sparkles: add variants, clsx and tailwind-merge
    add color variants, tailwind merge, clsx and nivo/pie

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,48 @@ export default {
     },
     extend: {
       gridTemplateColumns: {
-        app: 'minmax(18rem, 22rem) 1fr',
+        app: 'minmax(18rem, 20rem) 1fr',
+        card: '1fr 26.25rem',
+      },
+      borderRadius: {
+        20: '1.25rem',
       },
       colors: {
-        primary: '#05004E',
+        dark: '#333333',
+
+        primary: {
+          200: '#7B91B0',
+          900: '#05004E',
+          800: '#4079ED',
+          'dark-shade': '#05004E',
+        },
+
+        supportingColor: {
+          green: '#3CD856',
+          'green-shade': '#E2FFF3',
+
+          'violet-shade': '#FBF1FF',
+
+          yellow: '#FFA412',
+          'yellow-shade': '#FFFAF1',
+        },
+
+        green: {
+          50: '#F0FDF4',
+          100: '#DCFCE7',
+        },
+
+        lightBlue: {
+          50: '#F0F9FF',
+        },
 
         greys: {
-          900: '#151D48',
+          50: '#F0F0F0',
+          100: '#FFFFFF',
+          300: '#F9FAFB',
           700: '#737791',
+          800: '#444A6D',
+          900: '#151D48',
         },
       },
     },
